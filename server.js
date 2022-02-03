0365a09a8a4cee25534cda7b41f5b5a97d9c8e40
Fabiola Tempini <fabiola.tempini@gmail.com>
2022-02-02 21:14:26 -0300

    Third Commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express();
+const exphbs = require('express-handlebars');
+const { dirs } = require('nodemon/lib/config');
+
+const productosNombres = ['banana', 'cebollas', 'lechuga', 'papas', 'pimenton', 'tomate']
+
+app.set('view engine', 'handlebars');
+
+app.use('/', express.static(__dirname + '/assets'))
+app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist/css'))
+app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist'))
+app.use('/bootstrapJS', express.static(__dirname + '/node_modules/bootstrap/dist/js/'))
+
+
+app.engine('handlebars', exphbs.engine({
+    layoutsDir: __dirname + '/views/layouts',
+    partialsDir: __dirname + '/views/components',
+    helpers: { mensajeBienvenida: () => `Bienvenido(a) al mercado WEB, seleccione los productos que desea llevar` },
+}))
+
+app.get('/', (_, res) => {
+    res.render('Dashboard', {
+        productos: productosNombres,
+    });
+});
+
+app.listen(3000, () => {
+    console.log(`SERVER ON`)
+});
